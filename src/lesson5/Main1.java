25efdcea8f7d65628741c1a370ae945928b041f1
daniiladamov19@gmail.com <daniiladamov19@gmail.com>
2022-04-23 16:01:31 +0300

    тестовый sout в lesson5>>Main1

--- src/lesson5/Main1.java
+++ src/lesson5/Main1.java
@@ -3,13 +3,14 @@ package lesson5;
 public class Main1 {
     public static void main(String[] args) {
 
-        int count = StringUtils.countCharsFromText("wqdwqfewfewregbfd", 'w');
+        int count = StringUtils.countCharsFromText("лулуул", 'w');
         System.out.println(count);
 
         StringUtils.printReverse("fdsfewr");
 
 
 
+        System.out.println("git test");
 
     }
 }
